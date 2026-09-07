90d38e0d9d0fcba4afa1060faa0a7ce732c9922b
ssuyeh <ssuyehwork@gmail.com>
2026-09-07 10:29:11 +0700

    Sync: 2026-09-07 10:29:11

--- src/ui/PanelLayoutManager.h
+++ src/ui/PanelLayoutManager.h
@@ -35,10 +35,13 @@ public:
     bool isPanelVisible(const QString& panelId) const;
     bool isImmersiveMode() const;
     void toggleImmersiveMode();
-    void populatePanelMenu(QMenu* menu);
     void showPanelContextMenu(const QPoint& globalPos);
-    void updateDynamicMinimumSize();
     void saveLayoutState();
+
+private:
+    void applyDeferredLayoutRestore();
+    void populatePanelMenu(QMenu* menu);
+    void updateDynamicMinimumSize();
 
 private:
     void savePreImmersiveState();
